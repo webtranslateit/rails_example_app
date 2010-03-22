2d6b5fa8828fd5fa3b0ba3c3257a16088c6dcfb2
Édouard Brière <edouard.briere@gmail.com>
2010-03-22 19:31:05 +0100

    A better working example (for WebTranslateIt 1.6)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,17 @@
 class ApplicationController < ActionController::Base
   helper :all # include all helpers, all the time
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
+  
+  before_filter :update_locale
 
+  def update_locale
+    begin
+      WebTranslateIt.fetch_translations
+    rescue Exception => e
+      puts "** Web Translate It raised an exception: " + e.message
+    end
+  end
+  
   # Scrub sensitive parameters from your log
   # filter_parameter_logging :password
 end
